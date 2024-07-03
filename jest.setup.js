33273eee3c7625673196a1d6a9b95ce8d8bdfc6d
Diego Soto <88800678+DiegoLeonardoSoto@users.noreply.github.com>
2024-07-03 16:12:39 -0300

    add calendarApi test

---
+++ jest.setup.js
@@ -0,0 +1,19 @@
+// En caso de necesitar la implementación del FetchAPI
+// npm i -D whatwg-fetch
+// import 'whatwg-fetch';
+
+// En caso de encontrar paquetes que lo requieran
+// npm i -D setimmediate
+// import 'setimmediate';
+
+// En caso de tener variables de entorno y aún no soporta el import.meta.env
+// npm i -D dotenv
+require('dotenv').config({
+  path: '.env.test'
+})
+
+// Realizar el mock completo de las variables de entorno
+jest.mock('./src/helpers/getEnvVariables', () => ({
+  // eslint-disable-next-line no-undef
+  getEnvVariables: () => ({ ...process.env })
+}))
---
+++ tests/api/calendarApi.test.js
@@ -0,0 +1,16 @@
+import calendarApi from '../../src/api/calendarApi'
+
+describe('test on calendarApi', () => {
+  test('should have default configuration', () => {
+    // eslint-disable-next-line no-undef
+    expect(calendarApi.defaults.baseURL).toBe(process.env.VITE_API_URL)
+  })
+
+  test('should have x-token as a header on all requests', async () => {
+    const token = 'ABC-123-XYZ'
+
+    localStorage.setItem('token', token)
+    const res = await calendarApi.get('/auth')
+    expect(res.config.headers['x-token']).toBe(token)
+  })
+})
